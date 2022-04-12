b6c57a98b37ae343087fc30fd940f07f75e46b10
Maxim Yelkin <22443261+maxsimych@users.noreply.github.com>
2022-04-12 14:46:57 +0300

    temporary iv fix

--- src/utils/decrypt.ts
+++ src/utils/decrypt.ts
@@ -1,5 +1,4 @@
 import crypto from 'crypto';
-import Buffer from 'buffer';
 import { logger } from './logger';
 
 const ALGORITHM = 'aes128';
@@ -7,11 +6,13 @@ const OUTPUT_ENCODING = 'utf8';
 const IV_LENGTH = 16;
 
 export default function decrypt(encrypted: Buffer, key: string) {
-  const ivStart = encrypted.length - IV_LENGTH;
-  const iv = encrypted.subarray(ivStart);
-  logger.info('iv is:');
-  logger.info(iv);
-  const data = encrypted.subarray(0, ivStart);
+  // TODO generate iv and concat it to the ciphertext, when set up MTU
+  // const ivStart = encrypted.length - IV_LENGTH;
+  // const iv = encrypted.subarray(ivStart);
+  // const data = encrypted.subarray(0, ivStart);
+  const ivString = process.env.ENCRYPTION_IV || 'QfTjWnZr4u7x!A%C';
+  const iv = Buffer.from(ivString);
+  const data = encrypted;
   const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
   let decrypted = decipher.update(data, undefined, OUTPUT_ENCODING);
   decrypted += decipher.final(OUTPUT_ENCODING);
